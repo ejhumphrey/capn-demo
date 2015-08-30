c4370750193bfb4e1c5652cf95c61fb6831f243b
Eric J. Humphrey <ejhumphrey@nyu.edu>
2015-08-30 11:05:05 -0400

    Updated shell script to take a number of integer elements for populating the proto. For whatever reason, num_elements=3377 causes the libc++abi.dylib issue.

--- main.cpp
+++ main.cpp
@@ -1,27 +1,24 @@
 // basic file operations
 #include <iostream>
 #include <fstream>
-#include "math.h"
 #include "src/test.capnp.h"
 #include <capnp/message.h>
 #include <capnp/serialize-packed.h>
 
 #define FNAME "data/tmp.cpn"
-#define POWOF2 12
 
-const int kNumElements = (int)pow(2, POWOF2);
 using namespace std;
 
 
-void writeMultiArray(int fd) {
+void writeMultiArray(int fd, int num_elements) {
   ::capnp::MallocMessageBuilder message;
   MultiArray::Builder ndarray = message.initRoot<MultiArray>();
   ::capnp::List<Row>::Builder rows = ndarray.initRows(1);
 
   Row::Builder row = rows[0];
 
-  ::capnp::List<int>::Builder values = row.initValues(kNumElements);
-  for (int idx = 0; idx < kNumElements; ++idx)
+  ::capnp::List<int>::Builder values = row.initValues(num_elements);
+  for (int idx = 0; idx < num_elements; ++idx)
   {
       values.set(idx, idx);
   }
@@ -29,22 +26,27 @@ void writeMultiArray(int fd) {
 }
 
 
-void printMultiArray(int fd) {
+void printMultiArray(int fd, int num_elements) {
   ::capnp::PackedFdMessageReader message(fd);
   MultiArray::Reader ndarray = message.getRoot<MultiArray>();
   for (Row::Reader row : ndarray.getRows()) {
-    std::cout << ">> Number of elements (exp): " << kNumElements << std::endl;
+    std::cout << ">> Number of elements (exp): " << num_elements << std::endl;
     std::cout << ">> Number of elements (act): " << row.getValues().size() << std::endl;
   }
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc != 2)
+  {
+      std::cout << "Requires a `num_elements` argument." << std::endl;
+  }
+  int num_elements = atoi(argv[1]);
   FILE* write_handle = ::fopen(FNAME, "w");
-  writeMultiArray(fileno(write_handle));
+  writeMultiArray(fileno(write_handle), num_elements);
   ::fclose(write_handle);
 
   FILE* read_handle = ::fopen(FNAME, "r");
-  printMultiArray(fileno(read_handle));
+  printMultiArray(fileno(read_handle), num_elements);
   ::fclose(read_handle);
 
   return 0;
--- test_pycapnp.py
+++ test_pycapnp.py
@@ -1,18 +1,26 @@
+import argparse
 import capnp
-import test_capnp
 import numpy as np
+import test_capnp
 
 FNAME = "data/tmp.cpn"
 DTYPE = np.int32
-POWOF2 = 12
-NUM_ELEMENTS = 2**POWOF2
 
 
-def test_reading():
+def test_reading(num_elements):
     ndarray = test_capnp.MultiArray.read_packed(open(FNAME, 'r'))
     assert ndarray, "Failed reading {}".format(FNAME)
     x = ndarray.rows[0]
     assert x, "Doesn't exist!"
     np.testing.assert_equal(
         np.asarray(x.values, dtype=DTYPE),
-        np.arange(NUM_ELEMENTS))
+        np.arange(num_elements))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("num_elements",
+                        metavar="num_elements", type=int,
+                        help="Number of expected elements in the protobuff.")
+    args = parser.parse_args()
+    test_reading(args.num_elements)
