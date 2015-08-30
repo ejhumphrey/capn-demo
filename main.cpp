8e8ef8f6f50b99eb7e8c18eafa1e4e2046a913ff
Eric J. Humphrey <ejhumphrey@nyu.edu>
2015-08-29 20:35:17 -0400

    Stripped out unnecessary functionality, now just repeat int data; configured to failure on current platform (OS X 10.10.3)

--- main.cpp
+++ main.cpp
@@ -6,98 +6,46 @@
 #include <capnp/message.h>
 #include <capnp/serialize-packed.h>
 
-#define FNAME "temp.data"
+#define FNAME "data/tmp.cpn"
+#define POWOF2 12
 
+const int kNumElements = (int)pow(2, POWOF2);
 using namespace std;
 
-void writeAddressBook(int fd) {
+
+void writeMultiArray(int fd) {
   ::capnp::MallocMessageBuilder message;
+  MultiArray::Builder ndarray = message.initRoot<MultiArray>();
+  ::capnp::List<Row>::Builder rows = ndarray.initRows(1);
 
-  AddressBook::Builder addressBook = message.initRoot<AddressBook>();
-  ::capnp::List<Person>::Builder people = addressBook.initPeople(2);
+  Row::Builder row = rows[0];
 
-  Person::Builder alice = people[0];
-  alice.setId(123);
-  alice.setName("Alice");
-  alice.setEmail("alice@example.com");
-  // Type shown for explanation purposes; normally you'd use auto.
-  ::capnp::List<Person::PhoneNumber>::Builder alicePhones =
-      alice.initPhones(1);
-  alicePhones[0].setNumber("555-1212");
-  alicePhones[0].setType(Person::PhoneNumber::Type::MOBILE);
-  alice.getEmployment().setSchool("MIT");
-
-  int num_elements = (int)pow(2, 8);
-  ::capnp::List<int>::Builder larry = alice.initArray(num_elements);
-  // int *array = new int[num_elements];
-  for (int idx = 0; idx < num_elements; ++idx)
+  ::capnp::List<int>::Builder values = row.initValues(kNumElements);
+  for (int idx = 0; idx < kNumElements; ++idx)
   {
-      // array[idx] = idx;
-      larry.set(idx, idx);
-      // std::cout << array[idx] << std::endl;
+      values.set(idx, idx);
   }
-
-  // kj::ArrayPtr<kj::byte const> data(reinterpret_cast<kj::byte const*>(array),
-                                    // num_elements * sizeof(int));
-  // std::cout << "Sizeof int: " << sizeof(int) << std::endl;
-  // alice.setData(data);
-  // alice.initShape(1);
-
-  Person::Builder bob = people[1];
-  bob.setId(456);
-  bob.setName("Bob");
-  bob.setEmail("bob@example.com");
-  auto bobPhones = bob.initPhones(2);
-  bobPhones[0].setNumber("555-4567");
-  bobPhones[0].setType(Person::PhoneNumber::Type::HOME);
-  bobPhones[1].setNumber("555-7654");
-  bobPhones[1].setType(Person::PhoneNumber::Type::WORK);
-  bob.getEmployment().setUnemployed();
-
   writePackedMessageToFd(fd, message);
 }
 
-void printAddressBook(int fd) {
+
+void printMultiArray(int fd) {
   ::capnp::PackedFdMessageReader message(fd);
-
-  AddressBook::Reader addressBook = message.getRoot<AddressBook>();
-
-  for (Person::Reader person : addressBook.getPeople()) {
-    std::cout << person.getName().cStr() << ": "
-              << person.getEmail().cStr() << std::endl;
-    for (Person::PhoneNumber::Reader phone: person.getPhones()) {
-      const char* typeName = "UNKNOWN";
-      switch (phone.getType()) {
-        case Person::PhoneNumber::Type::MOBILE: typeName = "mobile"; break;
-        case Person::PhoneNumber::Type::HOME: typeName = "home"; break;
-        case Person::PhoneNumber::Type::WORK: typeName = "work"; break;
-      }
-      std::cout << "  " << typeName << " phone: "
-                << phone.getNumber().cStr() << std::endl;
-    }
-    Person::Employment::Reader employment = person.getEmployment();
-    switch (employment.which()) {
-      case Person::Employment::UNEMPLOYED:
-        std::cout << "  unemployed" << std::endl;
-        break;
-      case Person::Employment::EMPLOYER:
-        std::cout << "  employer: "
-                  << employment.getEmployer().cStr() << std::endl;
-        break;
-      case Person::Employment::SCHOOL:
-        std::cout << "  student at: "
-                  << employment.getSchool().cStr() << std::endl;
-        break;
-      case Person::Employment::SELF_EMPLOYED:
-        std::cout << "  self-employed" << std::endl;
-        break;
-    }
+  MultiArray::Reader ndarray = message.getRoot<MultiArray>();
+  for (Row::Reader row : ndarray.getRows()) {
+    std::cout << ">> Number of elements (exp): " << kNumElements << std::endl;
+    std::cout << ">> Number of elements (act): " << row.getValues().size() << std::endl;
   }
 }
 
 int main() {
-  FILE* file_handle = ::fopen(FNAME, "w");
-  writeAddressBook(fileno(file_handle));
-  ::fclose(file_handle);
+  FILE* write_handle = ::fopen(FNAME, "w");
+  writeMultiArray(fileno(write_handle));
+  ::fclose(write_handle);
+
+  FILE* read_handle = ::fopen(FNAME, "r");
+  printMultiArray(fileno(read_handle));
+  ::fclose(read_handle);
+
   return 0;
 }
--- test_pycapnp.py
+++ test_pycapnp.py
@@ -2,16 +2,17 @@ import capnp
 import test_capnp
 import numpy as np
 
-FNAME = "temp.data"
+FNAME = "data/tmp.cpn"
 DTYPE = np.int32
-NUM_ELEMENTS = 2**8
+POWOF2 = 12
+NUM_ELEMENTS = 2**POWOF2
 
 
 def test_reading():
-    x = test_capnp.AddressBook.read_packed(open(FNAME, 'r'))
-    assert x, "Failed reading {}".format(FNAME)
-    a = x.people[0]
-    assert a
+    ndarray = test_capnp.MultiArray.read_packed(open(FNAME, 'r'))
+    assert ndarray, "Failed reading {}".format(FNAME)
+    x = ndarray.rows[0]
+    assert x, "Doesn't exist!"
     np.testing.assert_equal(
-        np.asarray(a.array, dtype=DTYPE),
+        np.asarray(x.values, dtype=DTYPE),
         np.arange(NUM_ELEMENTS))
